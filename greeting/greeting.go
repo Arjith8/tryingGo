c809b1994f0d8560bc0798183f17174d26db01a4
Arjithar8 <arjithar8@gmail.com>
2025-02-01 20:37:51 +0530

    test

---
+++ greeting/greeting.go
@@ -0,0 +1,8 @@
+package greeting
+
+import "fmt"
+
+func helo(name string) string{
+    message := fmt.Sprintf("edeudheue %v", name);
+    return message
+}
---
+++ hello.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+	"github.com/Arjith8/tryingGo.git/greeting"
+	"rsc.io/quote"
+)
+
+func main(){
+    fmt.Println(quote.Glass());
+    greeting.helo()
+}
